2561db41a59d253bf98da55a2b0e4760da93a250
yonseung kim <ykim@yonseungs-MacBook-Pro.local>
2017-07-07 14:15:47 -0400

    add vaildation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end

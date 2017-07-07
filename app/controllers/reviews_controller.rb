1d7a1f4ce28c87495ecb80bded6f3b4bb3fc816d
yonseung kim <ykim@yonseungs-MacBook-Pro.local>
2017-07-07 02:11:56 -0400

    implement review form and controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,20 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.product_id = params[:product_id]
+
+    if @review.save
+      redirect_to '/products/' + params[:product_id], notice: 'Review created!'
+    else
+      redirect_to '/products/' + params[:product_id]
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
   end
 end
